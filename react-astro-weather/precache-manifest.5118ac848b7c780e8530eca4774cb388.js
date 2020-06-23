b992d9173773fc1ea5a286f44f7683cfd414f9a7
Vlad <astr0xy1@gmail.com>
2020-06-23 19:11:30 +0300

    добавил react-astro-weather

---
+++ react-astro-weather/precache-manifest.5118ac848b7c780e8530eca4774cb388.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086bb5cf260fda6c04fcd0943b2901ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1adcbae63f093fc0f2c3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b1990fde1ca173c9cc90",
+    "url": "/static/css/main.57efe851.chunk.css"
+  },
+  {
+    "revision": "1adcbae63f093fc0f2c3",
+    "url": "/static/js/2.76b9b09e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.76b9b09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1990fde1ca173c9cc90",
+    "url": "/static/js/main.c713b09b.chunk.js"
+  },
+  {
+    "revision": "cfd6cfd1b63b0cea9c5c",
+    "url": "/static/js/runtime-main.ba9233e9.js"
+  },
+  {
+    "revision": "735baf439613cceb550af55222af9bce",
+    "url": "/static/media/barometer.735baf43.svg"
+  },
+  {
+    "revision": "105823fb20ed892599ae19d4a300054d",
+    "url": "/static/media/cloud.105823fb.svg"
+  },
+  {
+    "revision": "493422edfa87b90f8d4ba1aa5a48b096",
+    "url": "/static/media/drop.493422ed.svg"
+  },
+  {
+    "revision": "79514ad176236bc98298ccdb09e5dbf6",
+    "url": "/static/media/search.79514ad1.svg"
+  },
+  {
+    "revision": "1adcb502d80af730a2caae881ee832a5",
+    "url": "/static/media/sunrise.1adcb502.svg"
+  },
+  {
+    "revision": "376fb81463ff153e8b96c9220e0de13a",
+    "url": "/static/media/sunset.376fb814.svg"
+  },
+  {
+    "revision": "f435e6d9a8cefd6eaf82500eacf44df4",
+    "url": "/static/media/wind.f435e6d9.svg"
+  }
+]);
