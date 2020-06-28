6afb399f815b069cfdda9511703c4b9ac60b10e9
Vlad <astr0xy1@gmail.com>
2020-06-28 20:45:11 +0300

    поправил билд astronauts и добавил скрин

---
+++ astronauts/precache-manifest.2fadeea70390599f5c35186fcfc406f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd0134ff98b84f563a138b5149aec4d",
+    "url": "/astronauts/index.html"
+  },
+  {
+    "revision": "2b717e4adbc71113a7a2",
+    "url": "/astronauts/static/css/main.af9c0f18.chunk.css"
+  },
+  {
+    "revision": "334bbba5b1a0cb0867e5",
+    "url": "/astronauts/static/js/2.69ddf902.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/astronauts/static/js/2.69ddf902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b717e4adbc71113a7a2",
+    "url": "/astronauts/static/js/main.104392a4.chunk.js"
+  },
+  {
+    "revision": "09f65df6bfca64e5eb36",
+    "url": "/astronauts/static/js/runtime-main.9603f1b7.js"
+  },
+  {
+    "revision": "02b9e3741d3fc3217be90d3c8ff1fb3f",
+    "url": "/astronauts/static/media/delete.02b9e374.svg"
+  }
+]);
